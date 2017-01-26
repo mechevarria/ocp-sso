1aa972f51173de6d58aa73a04167b7718e8c921e
redhat <mike.echevarria@gmail.com>
2017-01-26 11:30:54 -0500

    rename confusing test endpoint to status endpoint

---
+++ src/org/redhat/api/status/StatusController.java
@@ -0,0 +1,24 @@
+package org.redhat.api.status;
+
+import java.util.HashMap;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("status")
+@Produces(MediaType.APPLICATION_JSON)
+public class StatusController {
+	
+	@GET
+	public HashMap<String,String> isUp() {
+		
+		HashMap<String,String> model = new HashMap<String, String>();
+		model.put("status", "The service is up!");
+		
+		return model;
+		
+	}
+}
