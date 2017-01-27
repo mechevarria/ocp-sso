38a9abd4fa787fc8c299b8b2351077317f784ade
Mike Echevarria <mike.echevarria@gmail.com>
2017-01-27 10:37:14 -0500

    add call to status jboss-api endpoint

--- app/components/home/home-controller.js
+++ app/components/home/home-controller.js
@@ -4,9 +4,17 @@
     angular.module('jboss-client')
         .controller('HomeCtrl', Controller);
 
-    Controller.$inject = ['$scope'];
+    Controller.$inject = ['$scope', 'HomeSrvc'];
 
-    function Controller($scope) {
-        $scope.welcome = 'Welcome to the jboss-client-app!';
+    function Controller($scope, HomeSrvc) {
+        $scope.status = {};
+
+        $scope.check = function() {
+            HomeSrvc.status().then(function(res) {
+                $scope.status = res;
+            }, function(err) {
+                $scope.status = err;
+            });
+        }
     }
 })();
