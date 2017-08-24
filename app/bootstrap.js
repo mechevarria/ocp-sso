ac7a5019e2a08fc558395ae9773c67745c3571da
Mike Echevarria <mike.echevarria@gmail.com>
2017-08-24 09:01:02 -0400

    refactor callback chains and remove hardcoded values from logout url

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -1,26 +1,33 @@
 (function () {
     'use strict';
 
-    angular.element(document).ready(function () {
-        var keycloak = Keycloak();
+    var keycloak = Keycloak();
 
+    angular.element(document).ready(function () {
         keycloak.init({onLoad: 'login-required'})
-            .success(function () {
-                keycloak.loadUserProfile()
-                    .success(function (profile) {
-                        angular.module('patternfly.app').factory('AuthSrvc', function () {
-                            return {
-                                keycloak: keycloak,
-                                profile: profile,
-                                logoutUrl: keycloak.authServerUrl + '/realms/eap-node-realm/protocol/openid-connect/logout?redirect_uri=http://localhost:8181'
-                            };
-                        });
-                        angular.bootstrap(document, ['patternfly.app']);
-                    });
-            })
-            .error(function () {
-                window.location.reload();
-            });
+            .success(loadProfile)
+            .error(reload);
     });
+
+    function loadProfile() {
+        keycloak.loadUserProfile()
+            .success(saveAuthLoadAngular)
+            .error(reload);
+    }
+
+    function saveAuthLoadAngular(profile) {
+        angular.module('patternfly.app').factory('AuthSrvc', function () {
+            return {
+                keycloak: keycloak,
+                profile: profile,
+                logoutUrl: keycloak.authServerUrl + '/realms/' + keycloak.realm + '/protocol/openid-connect/logout?redirect_uri=' + window.location.href
+            };
+        });
+        angular.bootstrap(document, ['patternfly.app']);
+    }
+
+    function reload() {
+        window.location.reload();
+    }
 
 })();
