eb9d7fbdf8d8b463c0ee1651a11aacd179606cf9
Mike Echevarria <mechevar@redhat.com>
2018-03-02 12:34:35 -0500

    use card module from patternfly-ng

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {CardConfig, CardFilter} from 'patternfly-ng';
 import {MessageService} from '../services/message.service';
 
 @Component({
@@ -7,13 +8,37 @@ import {MessageService} from '../services/message.service';
 })
 export class CardComponent implements OnInit {
 
-  name = 'Card View';
+  basicConfig: CardConfig;
+  filterConfig: CardConfig;
+  selectedFilter = 'none';
 
   constructor(private messageService: MessageService) {
   }
 
   ngOnInit() {
-    this.messageService.success(`Successfully changed route to ${this.name}`);
+    this.messageService.success('Successfully changed route to Card View');
+
+    this.basicConfig = {
+      title: 'Basic Card'
+    } as CardConfig;
+
+    this.filterConfig = {
+      title: 'Filter Card',
+      filters: [{
+        title: 'Last 30',
+        value: '30'
+      }, {
+        title: 'Last 15',
+        value: '15'
+      }, {
+        title: 'Last 10',
+        value: '10'
+      }],
+    } as CardConfig;
+  }
+
+  handleFilter($event: CardFilter): void {
+    this.selectedFilter = $event.value;
   }
 
 }
