cf8f3d319137ae0e8a7815429aff021e277fe3cf
Mike Echevarria <mechevar@redhat.com>
2018-06-20 18:12:02 -0400

    use env replacement instead of datasource generation

--- s2i-run.sh
+++ s2i-run.sh
@@ -5,13 +5,8 @@ tag=s2i-build/jboss-api
 
 docker run \
 -p ${port}:${port} \
--e DB_SERVICE_PREFIX_MAPPING=db-postgresql=PG \
--e PG_DATABASE=jboss \
+-e PG_CONNECTION_URL=jdbc:postgresql://192.168.42.101:5432/jboss \
 -e PG_USERNAME=jboss \
 -e PG_PASSWORD=jboss \
--e DB_POSTGRESQL_SERVICE_HOST=192.168.42.101 \
--e DB_POSTGRESQL_SERVICE_PORT=5432 \
--e PG_DRIVER=postgresql \
--e PG_JNDI=java:/PostgresDS \
 -e AUTH_SERVER_URL=http://192.168.42.1:8180/auth \
 ${tag} \
