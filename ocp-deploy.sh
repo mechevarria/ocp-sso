07eff045200a0d5a34b8293e0ca52afdf0d7e670
Mike Echevarria <mechevar@redhat.com>
2018-07-30 15:13:44 -0400

    oc scripts to deploy

---
+++ ocp-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+oc login -u developer
+
+project="$(oc projects | grep nodejs-eap)"
+
+if [[ -z ${project} ]]; then
+  oc new-project nodejs-eap --display-name="node.js and EAP"
+else
+  oc project nodejs-eap
+fi
+
+ oc new-app https://github.com/mechevarria/jboss-client#keycloak \
+ --name=nodejs-app
+ 
+oc expose svc/nodejs-app
