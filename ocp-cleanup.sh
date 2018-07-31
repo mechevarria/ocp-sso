24751d7b84c643507c24e7f83b4059431cfe6181
Mike Echevarria <mechevar@redhat.com>
2018-07-30 20:52:51 -0400

    update scripts

--- ocp-cleanup.sh
+++ ocp-cleanup.sh
@@ -4,5 +4,7 @@ oc login -u developer
 
 oc project nodejs-eap
 oc delete all --selector app=eap-app
+oc delete dc/postgresql
 oc delete all --selector name=postgresql
 oc delete secret credentials
+oc delete secret postgresql
