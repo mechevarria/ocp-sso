820d5b54245b4f7ae8b54d8bedd6f729b838e5ae
Mike Echevarria <mechevar@redhat.com>
2018-08-01 15:55:21 -0400

    initial merge of repos

--- node/ocp-deploy.sh
+++ node/ocp-deploy.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-oc login -u developer
-
-=======
->>>>>>> keycloak
 project="$(oc projects | grep nodejs-eap)"
 
 if [[ -z ${project} ]]; then
-  oc new-project nodejs-eap --display-name="node.js and EAP"
+  oc new-project nodejs-eap --display-name="SSO N-Tier" --descrption="SSO secured node.js frontend, JBoss EAP backend and Postgresql datastorei with encrypted traffic"
 else
   oc project nodejs-eap
 fi
 
-oc new-app https://github.com/mechevarria/jboss-client \
- --name=nodejs-app
+oc new-app https://github.com/mechevarria/ocp-sso \
+--context-dir=node \
+--name=nodejs-app
  
 oc create route edge --service=nodejs-app --cert=server.cert --key=server.key
