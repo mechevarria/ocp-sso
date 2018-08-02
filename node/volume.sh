d9f42c6c18056a8dc40bafa66dc8f470aab3c6aa
Mike Echevarria <mechevar@redhat.com>
2018-08-02 11:21:45 -0400

    add override option

--- node/volume.sh
+++ node/volume.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+oc delete secret keycloak-secret
+
 oc secrets new keycloak-secret src/assets/data/keycloak.json
 
-oc volume dc/nodejs-app --add --name=keycloak-volume --type=secret --secret-name=keycloak-secret --mount-path=/opt/app-root/src/dist/assets/data
+oc volume dc/nodejs-app --add --name=keycloak-volume --type=secret --secret-name=keycloak-secret --mount-path=/opt/app-root/src/dist/assets/data --overwrite
