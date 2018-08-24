a7f45dfa241af295621402271d11c9b397da99e7
Mike Echevarria <mechevar@redhat.com>
2018-08-24 14:29:37 -0400

    add scripts for starting and stopping application

---
+++ scale-down.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+oc project ntier
+
+oc scale --replicas=0 dc nodejs-app
+oc scale --replicas=0 dc eap-app
+oc scale --replicas=0 dc postgresql
+oc scale --replicas=0 dc sso
+oc scale --replicas=0 dc sso-postgresql
---
+++ scale-up.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+oc project ntier
+
+oc scale --replicas=1 dc sso-postgresql
+sleep 2
+oc scale --replicas=1 dc sso
+oc scale --replicas=1 dc postgresql
+sleep 2
+oc scale --replicas=1 dc eap-app
+oc scale --replicas=1 dc nodejs-app
