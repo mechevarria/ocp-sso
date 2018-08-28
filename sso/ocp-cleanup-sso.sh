3497ca83a1a92966105186e9346301fd76169648
Mike Echevarria <mechevar@redhat.com>
2018-08-28 16:38:43 -0400

    use https-eap template and x509-sso template. remove unused certs

--- sso/ocp-cleanup-sso.sh
+++ sso/ocp-cleanup-sso.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 oc project ntier
-oc delete all --selector app=sso72-postgresql-persistent
+oc delete all --selector app=sso72-x509-postgresql-persistent
--- sso/ocp-deploy-sso.sh
+++ sso/ocp-deploy-sso.sh
@@ -10,22 +10,7 @@ fi
 
 oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default
 
-oc secret new sso-app-secret keystore.jks jgroups.jceks truststore.jks
-
-oc secrets link default sso-app-secret
-
-oc new-app sso72-postgresql-persistent \
- -p HTTPS_SECRET="sso-app-secret" \
- -p HTTPS_KEYSTORE="keystore.jks" \
- -p HTTPS_NAME="jboss" \
- -p HTTPS_PASSWORD="mykeystorepass" \
- -p JGROUPS_ENCRYPT_SECRET="sso-app-secret" \
- -p JGROUPS_ENCRYPT_KEYSTORE="jgroups.jceks" \
- -p JGROUPS_ENCRYPT_NAME="secret-key" \
- -p JGROUPS_ENCRYPT_PASSWORD="password" \
+oc new-app sso72-x509-postgresql-persistent \
  -p SSO_ADMIN_USERNAME="admin" \
  -p SSO_ADMIN_PASSWORD="Redhat1!" \
- -p SSO_REALM="eap-js-realm" \
- -p SSO_TRUSTSTORE="truststore.jks" \
- -p SSO_TRUSTSTORE_PASSWORD="mykeystorepass" \
- -p SSO_TRUSTSTORE_SECRET="sso-app-secret" \
+ -p SSO_REALM="eap-js-realm"
