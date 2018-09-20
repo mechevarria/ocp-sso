c3f108c1233f42b6fd66f906e3aacb2a03c2b99c
Mike Echevarria <mechevar@redhat.com>
2018-09-20 11:12:04 -0400

    add sprinboot to scaling, turn off security for testing

--- scale-down.sh
+++ scale-down.sh
@@ -3,6 +3,7 @@
 oc project ntier
 
 oc scale --replicas=0 dc nodejs-app
+oc scale --replicas=0 dc springboot-app
 oc scale --replicas=0 dc eap-app
 oc scale --replicas=0 dc postgresql
 oc scale --replicas=0 dc sso
--- scale-up.sh
+++ scale-up.sh
@@ -8,4 +8,5 @@ oc scale --replicas=1 dc sso
 oc scale --replicas=1 dc postgresql
 sleep 2
 oc scale --replicas=1 dc eap-app
+oc scale --replicas=1 dc springboot-app
 oc scale --replicas=1 dc nodejs-app
