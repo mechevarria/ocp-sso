94f936959c256197afe0476d039d00064e7cb13e
Mike Echevarria <mechevar@redhat.com>
2018-12-05 09:46:16 -0500

    change mysql driver and scale scripts for mysql

--- scale-down.sh
+++ scale-down.sh
@@ -6,6 +6,6 @@ oc project ${proj_name}
 oc scale --replicas=0 dc nodejs-app
 oc scale --replicas=0 dc springboot-app
 oc scale --replicas=0 dc eap-app
-oc scale --replicas=0 dc postgresql
+oc scale --replicas=0 dc mysql
 oc scale --replicas=0 dc sso
 oc scale --replicas=0 dc sso-postgresql
--- scale-up.sh
+++ scale-up.sh
@@ -6,7 +6,7 @@ oc project ${proj_name}
 oc scale --replicas=1 dc sso-postgresql
 sleep 2
 oc scale --replicas=1 dc sso
-oc scale --replicas=1 dc postgresql
+oc scale --replicas=1 dc mysql
 sleep 2
 oc scale --replicas=1 dc eap-app
 oc scale --replicas=1 dc springboot-app
