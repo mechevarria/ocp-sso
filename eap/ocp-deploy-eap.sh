260703108d71c7e2c7ec165c709a98456626c9b9
Mike Echevarria <mechevar@redhat.com>
2018-12-05 09:38:46 -0500

    use mysql as backend for eap. sso auth turned off for now

--- eap/ocp-deploy-eap.sh
+++ eap/ocp-deploy-eap.sh
@@ -4,28 +4,25 @@
 proj_name="$(oc whoami)-ntier"
 oc project ${proj_name}
 
-echo "Creating postgresql database"
+echo "Creating mysql database"
 
-db_service=postgresql
+db_service=mysql
 
 oc new-app \
---name=postgresql \
--p POSTGRESQL_USER=pguser \
--p POSTGRESQL_PASSWORD=pgpass \
--p POSTGRESQL_DATABASE=jboss \
--p POSTGRESQL_VERSION=9.5 \
+--name=mysql \
+-p MYSQL_USER=myuser \
+-p MYSQL_PASSWORD=mypass \
+-p MYSQL_DATABASE=jboss \
 -p DATABASE_SERVICE_NAME=${db_service} \
-postgresql-persistent
+mysql-persistent
 
-oc set env dc/${db_service} POSTGRESQL_MAX_PREPARED_TRANSACTIONS=10
-
-echo "Waiting for Postgresql to finish deploying before deploying EAP"
+echo "Waiting for mysql to finish deploying before deploying EAP"
 sleep 11
 
 oc new-app \
 --name=eap-app \
 -p SOURCE_REPOSITORY_URL=https://github.com/mechevarria/ocp-sso \
--p SOURCE_REPOSITORY_REF=master \
+-p SOURCE_REPOSITORY_REF=eap \
 -p CONTEXT_DIR=/eap \
 eap71-basic-s2i
 
