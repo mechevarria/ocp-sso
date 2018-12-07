a625f0321559f4a06f04fb31ce95f232c3d5c04e
Mike Echevarria <mechevar@redhat.com>
2018-12-07 14:26:16 -0500

    use rsync for now

---
+++ sso/rsync-theme.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# https://docs.okd.io/latest/dev_guide/copy_files_to_container.html
+
+pod_name=$(oc get pods --selector app=sso72-x509-postgresql-persistent | { read line1 ; read line2 ; echo "$line2" ; } | awk '{print $1;}')
+
+oc rsync themes/coreui ${pod_name}:/opt/eap/themes/
