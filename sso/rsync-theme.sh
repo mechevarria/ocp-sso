40cbcb18ce1753f2a07660e2c1af8ac3fe2b784f
Mike Echevarria <mechevar@redhat.com>
2019-03-14 10:40:54 -0400

    update from sso 7.3

--- sso/rsync-theme.sh
+++ sso/rsync-theme.sh
@@ -2,6 +2,6 @@
 
 # https://docs.okd.io/latest/dev_guide/copy_files_to_container.html
 
-pod_name=$(oc get pods --selector app=sso72-x509-postgresql-persistent | { read line1 ; read line2 ; echo "$line2" ; } | awk '{print $1;}')
+pod_name=$(oc get pods --selector app=sso73-x509-postgresql-persistent | { read line1 ; read line2 ; echo "$line2" ; } | awk '{print $1;}')
 
 oc rsync themes/coreui ${pod_name}:/opt/eap/themes/
